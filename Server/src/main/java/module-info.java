d6b86e8520b7607028fa83c1fc8d71e889c65cae
Sendariahi <senda.riahi@edu.unito.it>
2025-03-05 22:49:39 +0100

    server va

--- Server/src/main/java/module-info.java
+++ Server/src/main/java/module-info.java
@@ -2,11 +2,15 @@ module com.server {
     requires java.base;
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.base;
     requires javafx.graphics;
+    requires javafx.base;
     requires com.common;
 
     exports com.server;
     exports com.server.model;
     exports com.server.controller;
+
+    // Open packages for FXML if they contain controllers or views
+    opens com.server to javafx.fxml;
+    opens com.server.controller to javafx.fxml;
 }
